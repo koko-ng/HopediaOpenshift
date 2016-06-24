8f89f67536b3305f9b92e637b23290a76ae4cbed
koko-ng <koko-ng@users.noreply.github.com>
2016-06-24 16:19:26 +0200

    init

---
+++ index.js
@@ -0,0 +1,38 @@
+var api = require('hopedia_api'),
+    server = require('hopedia_web_server');
+var assert = require('assert');
+var mongo = require('mongodb').MongoClient;
+
+var url;
+var db_name='db'
+if(process.env.OPENSHIFT_MONGODB_DB_URL){
+	url = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
+}
+else
+	url = 'mongodb://localhost:27017/test';
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+mongo.connect(url, function(err, database) {
+	assert.equal(null, err);
+	console.log("Connected correctly to server.");
+	api({
+		database: database,
+		secret: '128-bit',
+		port:8081,
+		hostname:'localhost',
+	});
+
+	server({
+		database: database,
+		secret: '128-bit',
+		port:8080,
+		hostname:'localhost',
+		mailURI: process.env.OPENSHIFT_MAIL_URI
+	});
+});
+
+
+
+
